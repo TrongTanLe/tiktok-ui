9d8610abe4ee12f3aaf0485abddb6291b5b59d01
TrongTanLe <letrongtan201120011@gmail.com>
2024-02-24 22:04:43 +0700

    Add code layout sidebar #1

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,58 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
+import * as userService from '~/services/userService';
+import config from '~/config';
+import { useEffect, useState } from 'react';
+
+const cx = classNames.bind(styles);
+
+const INIT_PAGE = 1;
+const PER_PAGE = 5;
+
+function Sidebar() {
+    const [page, setPage] = useState([INIT_PAGE]);
+    const [suggestedUsers, setSuggestedUsers] = useState([]);
+
+    useEffect(() => {
+        userService
+            .getSuggested({ page, perPage: PER_PAGE })
+            .then((data) => {
+                setSuggestedUsers((prevUsers) => [...prevUsers, ...data]);
+            })
+            .catch((error) => console.log(error));
+    }, [page]);
+
+    const handleSeeAll = () => {
+        setPage(page + 1);
+    };
+
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+
+            <SuggestedAccounts label="Suggested accounts" data={suggestedUsers} onSeeAll={handleSeeAll} />
+            <SuggestedAccounts label="Following accounts" />
+        </aside>
+    );
+}
+
+export default Sidebar;
